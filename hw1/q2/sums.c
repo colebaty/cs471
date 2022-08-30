26ea526fed54a9b0473a61f276f5e25eef185afc
Cole <tbaty002@odu.edu>
2022-08-30 15:06:56 -0400

    fork() solution finished; starting q3

--- hw1/q2/sums.c
+++ hw1/q2/sums.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
+    pid_t pid1, pid2;
+    int sum1 = 0, sum2 = 0;
+    int n = atoi(argv[1]);
+
+    pid1 = fork();
+    if(pid1 < 0) // error occurred
+    {
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid1 == 0) // child process
+    {
+        sum1 = (n * (n + 1)) / 2; // formula for sum1 of first n ints
+        printf("The sum of the first %d natural numbers is: %d\n", n, sum1);
+
+        pid2 = fork();
+        if (pid2 < 0) // error occurred
+        {
+            printf("Second fork failed\n");
+        }
+        else if (pid2 == 0) // grandchild process
+        {
+            int i;
+            for (i = 1; i <= n; i++)
+            {
+                sum2 += i * i;
+            }
+
+            printf("The sum of the squares of the first %d natural numbers is: %d\n", n, sum2);
+        }
+    }
+    else // parent process
+    {
+        wait(NULL);
+    }
+
     return 0;
 }
