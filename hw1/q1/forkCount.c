c3bff2e548ea958c96e384f57cbd5d0525d4342a
Cole <tbaty002@odu.edu>
2022-09-01 14:26:18 -0400

    more tinkering

--- hw1/q1/forkCount.c
+++ hw1/q1/forkCount.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <wait.h>
+#include <math.h>
 
 //number of calls to fork()
 int count = 0;
 
 int main()
 {
+    int pid = getpid();
     /*fork a child process */
-    fork();
-    count++;
-    fork();
-    count++;
-    fork();
-    count++;
-    int i;
-    for(i = 0; i < 7; i++)
+
+    for (int i = 0; i < 11; i++)
     {
-        fork(); /* fork another child process */
+        fork();
         count++;
     }
-    fork();
-    count++;
 
-    printf("count: %d\n", count);
+    if (pid == getpid())
+    {
+        wait(NULL);
+        printf("num calls to fork(): %d\n", count);
+        printf("num processes created: %d\n", (int) pow(2, count));
+    }
 
     return 0;
 
