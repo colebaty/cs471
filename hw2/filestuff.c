7707aac5fca032a8c29c78cf4e0aa7f2d711bd55
cs_tbaty002 <tbaty002@.odu.edu>
2022-09-08 15:13:37 -0400

    queues work; file io works.  time to combine!

---
+++ hw2/filestuff.c
@@ -0,0 +1,37 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static struct Process {
+    int id;
+    int arr;
+    int burst;
+} *p;
+
+
+int main (int argc, char **argv) {
+
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    fp = fopen("data", "r");
+
+    //discard first line
+    getline(&line, &len, fp);
+    
+    //get rest of data
+    while ((nread = getline(&line, &len, fp)) != -1) {
+        fwrite(line, nread, 1, stdout);
+        p = malloc(sizeof(struct Process));
+        p->id = atoi(strtok(line, " \t"));
+        p->arr = atoi(strtok(NULL, " \t"));
+        p->burst = atoi(strtok(NULL, " \t"));
+        printf("Process %d: %d\t%d\n", p->id, p->arr, p->burst);
+    }
+
+    return 0;
+}
