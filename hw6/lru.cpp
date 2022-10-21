b5304a58eb51208cfd1b9475b6ccb8ae11d6c5c4
Cole <cole.baty@gmail.com>
2022-10-21 15:52:08 -0400

    it's working! it's working! -- anakin skywalker or dr. frankenstein

--- hw6/lru.cpp
+++ hw6/lru.cpp
@@ -14,6 +14,7 @@
  * 
  */
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 struct node {
@@ -23,9 +24,11 @@ struct node {
 }   *top, * bottom, *before, 
     *after, *curr, *temp; 
 
+int numFaults = 0;
+int numRefs = 0;
+
 using namespace std;
 
-void init();
 /**
  * @brief LRU stack management. 
  * 
@@ -63,12 +66,27 @@ void allocate(int page, int * f, int size);
  */
 int contains(int page, int *f, int size);
 
-int main()
+/**
+ * @brief determines if all available frames have been allocated
+ * 
+ * @param f 
+ * @param size 
+ * @return true 
+ * @return false 
+ */
+bool full(int *f, int size);
+
+int main(int argc, char **argv)
 {
-    int size = 3;
+    int size;
+    if (argc > 1) 
+        size = atoi(argv[1]);
+    else 
+        size = 3;
+
     int frames[size];
     for (int i = 0; i < size; i++)
-        frames[i] = -1;
+        frames[i] = -1; /* -1 == empty */
     
     vector<int> sequence = { 
         0, 3, 5, 1, 1, 2, 5, 
@@ -77,55 +95,48 @@ int main()
         10, 5, 6, 10, 1, 2, 3 
     };
 
+    cout << left 
+         << setw(20) << "Page reference #" 
+         << setw(20) << "Result" 
+         << setw(20) << "Frame allocated to" << endl;
+
     for (int i : sequence) {
         allocate(i, frames, size);
     }
 
+    cout << endl << "==================" << endl
+         << "Total # of references: " << numRefs << endl
+         << "Total # of page faults: " << numFaults << endl;
+
     return 0;
 }
 
-void init() {
-
-    for (int i = 0; i < 12; i++)
-    {
-        temp = new node();
-        temp->id = i;
-        if (i == 0) { /* first node */
-            top = bottom = temp;
-            top->next = bottom;
-            bottom->prev = top;
-        }
-        else {
-            temp->prev = bottom;
-            bottom->next = temp;
-            bottom = temp;
-        }
-    }
-}
-
 void ref(int page) {
-    /* search from bottom - LRU */
+    /* search from top. top is MRU, but locality means it's likeley to be on top */
     if (top != nullptr && bottom != nullptr) { /* if stack not empty */
         /* find page */
-        curr = bottom;
+        curr = top;
         while (curr != nullptr) {
             if (curr->id == page) break;
-            curr = curr->prev;
+            curr = curr->next;
         }
 
         if (curr != nullptr) { /* if page in stack */
-            /* move curr to top */
-            /* get neighbors */
-            before = curr->prev;
-            after = curr->next;
+            if (curr != top) { /* move curr to top */
+                /* get neighbors */
+                before = curr->prev;
+                after = curr->next;
 
-            before->next = after;
-            after->prev = before;
+                before->next = after;
+                if (after != nullptr) after->prev = before; /* if curr not last element */
+                if (curr == bottom) bottom = before;
 
-            curr->next = top;
-            curr->prev = nullptr;
-            top->prev = curr;
-            top = curr;
+                curr->next = top;
+                curr->prev = nullptr;
+                top->prev = curr;
+                top = curr;
+                curr = before = after = nullptr;
+            }
         }
         else { /* add to top */
             temp = new node();
@@ -134,12 +145,15 @@ void ref(int page) {
             top->prev = temp;
             temp->next = top;
             top = temp;
+            temp = nullptr;
         }
     }
     else { /* stack empty; add first element */
         temp = new node();
         temp->id = page;
-        top = bottom = temp;
+        top = temp;
+        bottom = temp;
+        temp = nullptr;
     }
 
 }
@@ -152,6 +166,8 @@ int lru(int * f, int size) {
         
         curr = curr->prev;
     }
+
+    return -1;
 }
 
 int contains(int page, int *f, int size) {
@@ -162,8 +178,39 @@ int contains(int page, int *f, int size) {
 }
 
 void allocate(int page, int * f, int size) {
-    if (contains(page, f, size) < 0) { /* if page not already allocated */
+    cout << setw(20) << page;
 
+    if (contains(page, f, size) > -1) { /* hit */
+        cout << setw(20) << "No Page Fault" << "F" << contains(page, f, size) << endl;
     }
-    ref(page);
+    else { /* miss */
+        cout << setw(20) << "Page Fault" << "F";
+        if (!full(f, size)) { /* if there are empty frames */
+            int i;
+            for (i = 0; i < size; i++) { /* populate */
+                if (f[i] == -1) {
+                    f[i] = page;
+                    break;
+                }
+            }
+            
+            cout << i << endl;
+        }
+        else { /* select a victim */
+            int victim = lru(f, size);
+            f[victim] = page;
+            cout << victim << endl;
+        }
+
+        numFaults++;
+    }
+
+    ref(page); /* update stack */
+    numRefs++;
+}
+
+bool full(int *f, int size) {
+    for (int i = 0; i < size; i++)
+        if (f[i] < 0) return false; /* if any frame is empty, return false */
+    return true;
 }
