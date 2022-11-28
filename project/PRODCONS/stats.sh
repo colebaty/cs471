8dbf9d6666d3a3e11949b33eba3a5c26ae37e070
cs_tbaty002 <tbaty002@.odu.edu>
2022-11-28 09:18:04 -0500

    script for compiling run stats to csv

---
+++ project/PRODCONS/stats.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+p=(2 5 10)
+c=(2 5 10)
+b=(2 10)
+
+echo "compiling statistics"
+
+echo "p,c,b,dur" > stats.csv
+
+for i in ${p[@]}; do
+    for j in ${c[@]}; do
+        for k in ${b[@]}; do
+            if [ ! -f "sample-$i-$j-$k" ]; then
+                echo "sample-$i-$j-$k not found; skipping"
+                continue
+            else
+                dur=$(tail -n1 "sample-$i-$j-$k" | cut -d' ' -f3)
+                echo "$i,$j,$k,$dur" >> stats.csv
+            fi
+        done
+    done
+done
