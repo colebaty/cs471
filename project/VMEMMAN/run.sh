31c0b9f60a6f0629d7e86b73fdce80f321a3f047
Cole Baty <tbaty002@odu.edu>
2022-11-30 11:59:03 -0500

    run.sh is ready

--- project/VMEMMAN/run.sh
+++ project/VMEMMAN/run.sh
@@ -1,15 +1,31 @@
  #!/usr/bin/bash
+
+ # automatically compile program and run for given sample values in $p and $f
+ # data is stored in file 'samples'
  
 p=(512 1024 2048)
 f=(4 8 12)
 
-echo > samples
+if [ ! -f main ]; then
+    echo "executable not found; compiling"
+    g++ -fpermissive -std=c++17 -g main.cpp -o main
+fi
 
- for i in ${p[@]}; do 
-    for j in ${f[@]}; do 
-        echo "=====| p = $i | f = $j |=====" >> samples
-        ./bin/main $i $j input | grep numrefs >> samples
-        echo "=============================" >> samples
-        echo >> samples; 
-    done; 
-done 
+if [ ! -f input ]; then
+    echo "no input file found"
+    echo "please place input file in same directory as executable"
+    echo "file must have name 'input'"
+else
+    echo "running simulations; data stored in file 'samples'"
+    echo > samples
+
+    for i in ${p[@]}; do 
+        for j in ${f[@]}; do 
+            echo "=====| p = $i | f = $j |=====" >> samples
+            ./main $i $j input >> samples
+            echo "=============================" >> samples
+            echo >> samples; 
+        done; 
+    done 
+    cat samples
+fi
