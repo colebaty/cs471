501c8f0556f36213fc2958eecf71c3b3ab9ab120
cs_tbaty002 <tbaty002@.odu.edu>
2022-11-30 14:38:14 -0500

    this must have been changed during the merge; testing on home machine

--- project/VMEMMAN/stats.sh
+++ project/VMEMMAN/stats.sh
@@ -2,18 +2,18 @@
 
 # automatically compile program and run for given sample values in $p and $f
 # data is stored in file 'samples'
- 
+
 p=(512 1024 2048)
 f=(4 8 12)
 
 gen_stats () {
-    echo "exporting sample data to samples.csv"
+    echo "exporting sample data to stats.csv"
 
     echo "p,f,num pgs,alg,fault ratio" > stats.csv
 
     for i in ${p[@]}; do 
         for j in ${f[@]}; do 
-            end=$(cat sample-$i-$j | grep -vi "page size" | awk '{ printf "%s,%s,%s\n", $2,$3,$4 }')
+            end=$(cat sample-1024-12 | grep -vi "page size" | awk '{ printf "%s,%s,%s\n", $2,$5,$10 }')
             for n in $end; do
                 echo "$i,$j,$n" >> stats.csv # page size, num frames
             done;
@@ -21,7 +21,7 @@ gen_stats () {
     done 
 }
 
-if [ ! -f sample* ]; then
+if [[ ! -f sample* ]]; then
     echo "no samples found; generating samples"
     ./run.sh
     gen_stats
